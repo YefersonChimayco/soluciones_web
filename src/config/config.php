0de75a7c933e024f3dcfcce1b12efd6652bf2307
YefersonChimayco <chimaycohnj@gmail.com>
2025-05-06 12:49:56 -0500

    clase-6dMayo

--- src/config/config.php
+++ src/config/config.php
@@ -1,14 +1,14 @@
 <?php
 
 const BD_HOST = 'localhost';
-const BD_NAME = '';
-const BD_USER = '';
-const BD_PASSWORD = '';
+const BD_NAME = 'inventario';
+const BD_USER = 'root';
+const BD_PASSWORD = 'root';
 
 const BD_CHARSET = 'utf8';
 
 //ingresar link de proyecto
-const BASE_URL = 'http://localhost:8888/inventario/';
-const BASE_URL_SERVER = 'http://localhost:8888/inventario/';
+const BASE_URL = 'http://localhost/soluciones_web/';
+const BASE_URL_SERVER = 'http://localhost/soluciones_web/';
 
 ?>
