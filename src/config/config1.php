1a968001a5e9a4bd339de74dfbc590c332933cf5
YefersonChimayco <chimaycohnj@gmail.com>
2025-08-03 23:26:34 -0500

    Update config1.php

--- src/config/config1.php
+++ src/config/config1.php
@@ -1,14 +1,14 @@
 <?php
 
 const BD_HOST = 'localhost';
-const BD_NAME = 'dpwebcom_chimasolu';
-const BD_USER = 'dpwebcom_chimasolu';
-const BD_PASSWORD = 'Gp3CMEz05dD';
+const BD_NAME = '';
+const BD_USER = '';
+const BD_PASSWORD = '';
 
 const BD_CHARSET = 'utf8';
 
 //ingresar link de proyecto
-const BASE_URL = 'http://inventariaochimayco.dpweb2024.com/';
-const BASE_URL_SERVER = 'https://inventariaochimayco.dpweb2024.com/';
+const BASE_URL = '';
+const BASE_URL_SERVER = '';
 
 ?>
